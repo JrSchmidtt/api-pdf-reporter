ff61ad44242189668117663a0068cb8afb9f735d
jrschmidtt <junior.schmidt.99@gmail.com>
2022-09-25 02:01:00 -0300

    add http api 	modified:   main.go

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 	"pdf-reporter/htmlParser"
 	"pdf-reporter/pdfGenerator"
@@ -11,7 +14,20 @@ type Data struct {
 	Name string
 }
 
+type Response struct {
+	Id int
+	FileName string
+}
+
 func main(){
+	http.HandleFunc("/pdf", getPDF)
+	fmt.Println("Api Running..")
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
+
+func getPDF(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content=Type", "application/json")
+
 	dataHtml := Data{
 		Name : "Lorem Ipsum",
 	}
@@ -33,4 +49,9 @@ func main(){
 		return
 	}
 	fmt.Println("File Pdf: ",filePDFName)
+	
+	json.NewEncoder(w).Encode([]Response{{
+		Id: 1,
+		FileName: filePDFName,
+	}})
 }
