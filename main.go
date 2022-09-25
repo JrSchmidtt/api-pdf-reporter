d10a9efd19f8bca6a1fba14ef383641ece459c6e
jrschmidtt <junior.schmidt.99@gmail.com>
2022-09-24 23:19:51 -0300

    add dinamic data

--- main.go
+++ main.go
@@ -5,9 +5,17 @@ import (
 	"pdf-reporter/htmlParser"
 )
 
+type Data struct {
+	Name string
+}
+
 func main(){
 	h := htmlParser.New("tmp")
-	htmlGenerated, err := h.Create("templates/example.html", nil)
+
+	dataHTML := Data{
+		Name : "Junior Schmidt",
+	}
+	htmlGenerated, err := h.Create("templates/example.html", dataHTML)
 	if err != nil {
 		fmt.Println(err)
 		return
